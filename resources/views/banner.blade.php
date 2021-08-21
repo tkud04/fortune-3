93bf359d0ef60704b26e9c20f07912087329224f
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-08-21 17:48:30 +0100

    Update banner.blade.php

--- resources/views/banner.blade.php
+++ resources/views/banner.blade.php
@@ -1,118 +1,32 @@
-<section class="intro-slider owl-carousel owl-theme owl-dot-inner animation-slider owl-loaded owl-drag" data-owl-options="{
-                                'items': 1,
-                                'loop': true,
-                                'autoplay': true,
-                                'animateIn': 'slideInDown'
-                            }">
-                                
-                                
-                                
-                            <div class="owl-stage-outer">
-							  <div class="owl-stage" style="transform: translate3d(-1760px, 0px, 0px); transition: all 0s ease 0s; width: 6160px;"><div class="owl-item cloned" style="width: 880px;"><div class="intro-slide2 banner banner-fixed" style="background-color: #5481ab" data-index="2">
-                                    <figure>
-                                        <img src="images/slide-2.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50 text-right">
-                                        <h4 class="banner-subtitle mb-2 text-white font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInDown',  'duration': '1s'}">New
-                                            Arrivals</h4>
-                                        <h3 class="banner-title ls-l text-white text-uppercase font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s'}">For
-                                            Women’s</h3>
-                                        <p class="text-white mb-6 ls-l font-weight-semi-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.1s', 'delay': '.5s'}">
-                                            Start at $12.00</p>
-                                        <a href="#" class="btn btn-dark slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s', 'delay': '.7s'}">Shop
-                                            now</a>
-                                    </div>
-                                </div></div><div class="owl-item cloned" style="width: 880px;"><div class="intro-slide3 banner banner-fixed" style="background-color: #dcdcdb" data-index="3">
-                                    <figure>
-                                        <img src="images/slide-3.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50 text-right">
-                                        <h4 class="banner-subtitle mb-0 text-uppercase font-weight-normal slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s'}">
-                                            Comfortable</h4>
-                                        <h3 class="banner-title ls-m text-uppercase slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.2s'}">
-                                            Sportswear</h3>
-                                        <p class="text-uppercase mb-5 ls-s slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.3s', 'delay': '.5s'}">
-                                            up to<span class="ml-2 text-primary font-weight-bold ls-m">30% Off</span>
-                                        </p>
-                                        <a href="#" class="btn btn-outline btn-dark slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.3s', 'delay': '.8s'}">Shop
-                                            now</a>
-                                    </div>
-                                </div></div><div class="owl-item active" style="width: 880px;"><div class="intro-slide1 banner banner-fixed" style="background-color: #f2f3f5" data-index="1">
-                                    <figure>
-                                        <img src="images/slide-1.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50">
-                                        <h4 class="banner-subtitle mb-2 text-body text-uppercase font-weight-normal slide-animate fadeInLeftShorter show-content" data-animation-options="{'name': 'fadeInLeftShorter', 'duration': '1s'}" style="animation-duration: 1s; animation-delay: 0.2s; transition-property: visibility, opacity; transition-delay: 0.2s; transition-duration: 1s;">
-                                            Natural Sound</h4>
-                                        <h3 class="banner-title mb-2 ls-m text-uppercase font-weight-bold slide-animate fadeInRightShorter show-content" data-animation-options="{'name': 'fadeInRightShorter', 'duration': '1.2s'}" style="animation-duration: 1.2s; animation-delay: 0.2s; transition-property: visibility, opacity; transition-delay: 0.2s; transition-duration: 1.2s;">
-                                            Senheiser <span class="text-primary">HD8 DJ</span></h3>
-                                        <p class="mb-5 text-grey ls-l slide-animate fadeInLeftShorter show-content" data-animation-options="{'name': 'fadeInLeftShorter', 'duration': '1.2s', 'delay': '.3s'}" style="animation-duration: 1.2s; animation-delay: 0.3s; transition-property: visibility, opacity; transition-delay: 0.3s; transition-duration: 1.2s;">
-                                            Intuitive operating experience</p>
-                                        <a href="#" class="btn btn-outline btn-dark slide-animate fadeIn show-content" data-animation-options="{'name': 'fadeIn', 'duration': '1.4s', 'delay': '.7s'}" style="animation-duration: 1.4s; animation-delay: 0.7s; transition-property: visibility, opacity; transition-delay: 0.7s; transition-duration: 1.4s;">Shop
-                                            now</a>
-                                    </div>
-                                </div></div><div class="owl-item" style="width: 880px;"><div class="intro-slide2 banner banner-fixed" style="background-color: #5481ab" data-index="2">
-                                    <figure>
-                                        <img src="images/slide-2.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50 text-right">
-                                        <h4 class="banner-subtitle mb-2 text-white font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInDown',  'duration': '1s'}" style="animation-duration: 1s; animation-delay: 0.2s;">New
-                                            Arrivals</h4>
-                                        <h3 class="banner-title ls-l text-white text-uppercase font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s'}" style="animation-duration: 1.2s; animation-delay: 0.2s;">For
-                                            Women’s</h3>
-                                        <p class="text-white mb-6 ls-l font-weight-semi-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.1s', 'delay': '.5s'}" style="animation-duration: 1.1s; animation-delay: 0.5s;">
-                                            Start at $12.00</p>
-                                        <a href="#" class="btn btn-dark slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s', 'delay': '.7s'}" style="animation-duration: 1.2s; animation-delay: 0.7s;">Shop
-                                            now</a>
-                                    </div>
-                                </div></div><div class="owl-item" style="width: 880px;"><div class="intro-slide3 banner banner-fixed" style="background-color: #dcdcdb" data-index="3">
-                                    <figure>
-                                        <img src="images/slide-3.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50 text-right">
-                                        <h4 class="banner-subtitle mb-0 text-uppercase font-weight-normal slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s'}" style="animation-duration: 1.2s; animation-delay: 0.2s;">
-                                            Comfortable</h4>
-                                        <h3 class="banner-title ls-m text-uppercase slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.2s'}" style="animation-duration: 1.2s; animation-delay: 0.2s;">
-                                            Sportswear</h3>
-                                        <p class="text-uppercase mb-5 ls-s slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.3s', 'delay': '.5s'}" style="animation-duration: 1.3s; animation-delay: 0.5s;">
-                                            up to<span class="ml-2 text-primary font-weight-bold ls-m">30% Off</span>
-                                        </p>
-                                        <a href="#" class="btn btn-outline btn-dark slide-animate" data-animation-options="{'name': 'fadeInDown', 'duration': '1.3s', 'delay': '.8s'}" style="animation-duration: 1.3s; animation-delay: 0.8s;">Shop
-                                            now</a>
-                                    </div>
-                                </div></div><div class="owl-item cloned" style="width: 880px;"><div class="intro-slide1 banner banner-fixed" style="background-color: #f2f3f5" data-index="1">
-                                    <figure>
-                                        <img src="images/slide-1.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50">
-                                        <h4 class="banner-subtitle mb-2 text-body text-uppercase font-weight-normal slide-animate" data-animation-options="{'name': 'fadeInLeftShorter', 'duration': '1s'}">
-                                            Natural Sound</h4>
-                                        <h3 class="banner-title mb-2 ls-m text-uppercase font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInRightShorter', 'duration': '1.2s'}">
-                                            Senheiser <span class="text-primary">HD8 DJ</span></h3>
-                                        <p class="mb-5 text-grey ls-l slide-animate" data-animation-options="{'name': 'fadeInLeftShorter', 'duration': '1.2s', 'delay': '.3s'}">
-                                            Intuitive operating experience</p>
-                                        <a href="#" class="btn btn-outline btn-dark slide-animate" data-animation-options="{'name': 'fadeIn', 'duration': '1.4s', 'delay': '.7s'}">Shop
-                                            now</a>
-                                    </div>
-                                </div></div>
-								<div class="owl-item cloned" style="width: 880px;">
-								  <div class="intro-slide2 banner banner-fixed" style="background-color: #5481ab" data-index="2">
-                                    <figure>
-                                        <img src="images/slide-2.jpg" alt="banner" width="880" height="450">
-                                    </figure>
-                                    <div class="banner-content y-50 text-right">
-                                        <h4 class="banner-subtitle mb-2 text-white font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInDown',  'duration': '1s'}">New
-                                            Arrivals</h4>
-                                        <h3 class="banner-title ls-l text-white text-uppercase font-weight-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s'}">For
-                                            Women’s</h3>
-                                        <p class="text-white mb-6 ls-l font-weight-semi-bold slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.1s', 'delay': '.5s'}">
-                                            Start at $12.00</p>
-                                        <a href="#" class="btn btn-dark slide-animate" data-animation-options="{'name': 'fadeInUp', 'duration': '1.2s', 'delay': '.7s'}">Shop
-                                            now</a>
-                                    </div>
-                                  </div>
-							    </div>
-							  </div>
-							 </div>
-							 <div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><i class="d-icon-angle-left"></i></button><button type="button" role="presentation" class="owl-next"><i class="d-icon-angle-right"></i></button></div><div class="owl-dots"><button role="button" class="owl-dot active"><span></span></button><button role="button" class="owl-dot"><span></span></button><button role="button" class="owl-dot"><span></span></button></div>
-							</section>
+<section id="block_60399ede58603" class="content-block d-flex  alignfull adtheme-hero align-items-sm-center align-items-end mob-stack color-as-herotype height-full has-background background-000000 has-background video-as-background has-font-color text-default">
+  <video class="video-background skip-lazy d-none d-md-block" autoplay muted  playsinline preload="auto" aria-hidden="true" poster="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">
+     <source src="images/Amsive-Hero-02_optimized.mp4" type="video/mp4">
+  </video>
+
+  <div class="image-background skip-lazy d-none d-md-block">
+	<picture class="skip-lazy object-cover object-center w-100 h-100" aria-hidden="true" loading="eager">
+      <source type="image/webp" srcset="images/Amsive-Hero-02.jpg.webp"/>
+      <img width="1920" height="1080" src="images/Amsive-Hero-02.jpg" aria-hidden="true" loading="eager"/>
+    </picture>
+  </div>
+  <div class="image-background skip-lazy d-block d-md-none">
+	<picture class="skip-lazy object-cover object-center w-100 h-100" aria-hidden="true" loading="eager">
+      <source type="image/webp" srcset="images/Amsive-Hero-02-mobile.jpg.webp 414w, images/Amsive-Hero-02-mobile-331x400.jpg.webp 331w, images/Amsive-Hero-02-mobile-166x200.jpg.webp 166w" sizes="(max-width: 414px) 100vw, 414px"/>
+      <img width="414" height="500" src="images/Amsive-Hero-02-mobile.jpg" alt="Amsive Hero Mobile" aria-hidden="true" loading="eager" srcset="images/Amsive-Hero-02-mobile.jpg 414w, images/Amsive-Hero-02-mobile-331x400.jpg 331w, images/Amsive-Hero-02-mobile-166x200.jpg 166w" sizes="(max-width: 414px) 100vw, 414px"/>
+    </picture>
+  </div>
+							
+  <button class="video-toggle"><span class="screen-reader-text">Toggle video playback</span></button>
+			
+  <div class="container-fluid mw-xl hero-block-content pt-sm-5 mt-sm-5">
+	<div class="row h-100">
+	  <div class="col-md-7 text-start my-sm-3 my-4 text-white ">
+		<h1 class="hero-title display-1">We’re your data-driven brand growth engine.</h1>						
+		<div class="d-none d-sm-block">
+		   <div class="hero-block-description py-3 h5">Amplify your business with our data-centric, performance-driven digital marketing solutions.</div>
+		   <a class="btn btn-secondary" href="/services/" target="_self">Explore</a>
+		</div>
+	  </div>
+    </div>		
+  </div>
+</section>

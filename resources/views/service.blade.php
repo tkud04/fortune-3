7e32ae4df683b7888df93f7ffcdc932075fae649
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-09-08 12:11:01 +0100

    Update service.blade.php

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -11,5 +11,30 @@ $pcClass = "";
 @section('bodyClass',"page-paid-media noawesome hashero-color hashero")
 
 @section('content')
-
+<section id="block_60458b644b028" class="content-block d-flex  alignfull adtheme-hero align-items-end mob-stack color-as-herotype height-def has-background background-000000 has-background image-as-background has-overlay has-font-color text-default">			
+<div class="image-background  hero-overlay background-000000 d-none d-sm-block">
+				<picture class="skip-lazy object-cover object-center w-100 h-100" aria-hidden="true" loading="eager">
+<source type="image/webp" srcset="images/services.jpg 1900w, images/services.jpg 1057w, images/services.jpg 1718w, images/services.jpg 529w, images/services.jpg 768w, images/services.jpg 1536w" sizes="(max-width: 1900px) 100vw, 1900px"/>
+<img width="1900" height="719" src="images/services.jpg" alt="Services" aria-hidden="true" loading="eager" srcset="images/services.jpg 1900w, images/services.jpg 1057w, images/services.jpg 1718w, images/services.jpg 529w, images/services.jpg 768w, images/services.jpg 1536w" sizes="(max-width: 1900px) 100vw, 1900px"/>
+</picture>
+			</div>
+<div class="image-background skip-lazy hero-overlay background-000000 d-none d-sm-block">
+				<picture class="skip-lazy object-cover object-center w-100 h-100" aria-hidden="true" loading="eager">
+<source type="image/webp" srcset="images/services.jpg 1900w, images/services.jpg 1057w, images/services.jpg 1718w, images/services.jpg 529w, images/services.jpg 768w, images/services.jpg 1536w" sizes="(max-width: 1900px) 100vw, 1900px"/>
+<img width="1900" height="719" src="images/services.jpg" alt="Services" aria-hidden="true" loading="eager" srcset="images/services.jpg 1900w, images/services.jpg 1057w, images/services.jpg 1718w, images/services.jpg 529w, images/services.jpg 768w, images/services.jpg 1536w" sizes="(max-width: 1900px) 100vw, 1900px"/>
+</picture>
+			</div>
+<div class="container-fluid mw-xl hero-block-content pt-sm-5 mt-sm-5">
+		<div class="row h-100">
+			<div class="col-md-7 text-start my-sm-3 my-4 text-white pb-md-5">
+																	<div class="label">Services</div>
+												<h1 class="hero-title display-1">We help brands grow.</h1>						
+				<div class="d-none d-sm-block">
+											<div class="hero-block-description py-3 h5">We drive business growth with hard-hitting digital solutions that help you win in hyper-competitive marketplaces with complex customer journeys.</div>
+														
+				</div>
+			</div>
+					</div>		
+	</div>
+</section>
 @stop

9205c7afbdfaf5f789eb44604f893563735507d3
Tkud <kudayisitobi@gmail.com>
2021-09-09 19:59:04 +0100

    rounding up

---
+++ storage/framework/views/62a378754ff3fba8a1744b8854a27c9d3d11cc2b.php
@@ -0,0 +1,156 @@
+<?php
+$pcClass = "";
+?>
+
+
+
+<?php $__env->startSection('title',"SEO"); ?>
+
+
+
+<?php $__env->startSection('bodyClass',"page-paid-media noawesome hashero-color hashero"); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php
+$sh = [
+  'subtitle' => "Social",
+  'title' => "Pioneering search engine optimization.",
+  'copy' => "Gain and maintain your place at the top of SERPs with our expertise on all leading SEO platforms and licensing to top-tier tools."
+];
+
+$sl = [
+  'title_1' => "what we do",
+  'title_2' => "Full funnel. Full force. Intelligent audience targeting and expert technical know-how to drive long-term growth.",
+  'title_3' => "",
+  'list' => [
+  ['title' => "Technical SEO",'copy' => "Ensure search engines can effectively crawl, index, and render your web content by improving efficiency, eliminating technical roadblocks, and optimizing performance and usability."],
+  ['title' => "Performance Content",'copy' => "Establish authority and relevance through impactful content and intent-based optimizations that speak directly to your customer."],
+  ['title' => "ECommerce",'copy' => "Boost visibility and sales with unique and relevant product information and eCommerce platform customization."],
+  ['title' => "Local SEO",'copy' => "Help neighborhood customers easily find your business with a targeted local content strategy, including listing and review management."],
+  ['title' => "Algorithm Recovery",'copy' => "With our finger on the pulse of future updates, we help you navigate changes and support renewed traffic growth."],
+  ]
+];
+
+$sc = [
+'subtitle' => "OUR APPROACH",
+  'title' => "Website redesign, relaunch + replatforming.",
+  'copy' => "<p>We use the latest SEO and digital marketing best practices to boost your brand. Our team supports yours, as much as is needed, throughout the entire process. </p><ul><li>Risk Analysis + Research</li><li>Sitemap + Wireframe + HTML Evaluation</li><li>Performance Content + Design</li><li>Optimization + Review</li><li>Website Development + Maintenance</li></ul>"
+];
+
+$scl = [
+  'img' => "images/phamatex-2.jpg",
+  'title' => "Phamatex Industries",
+  'num' => "+675%",
+  'subtitle' => "KPI social-wide click-throughs",
+  'copy' => "By utilizing Google SEO, we deployed Keyword Search for word strings closely matching popular search options of profiles of customers within target audience demographic. Conersions to social campaigns to generate MQLs grew by +65% during campaign period."
+];
+
+$sp = [
+  'title' => "We know the right people.",
+  'subtitle' => "OUR PARTNERS",
+  'copy' => "We’ve built elite partnerships and strong relationships with leading SEO institutions. These partnerships grant us enterprise-level technology and insights to know more, pivot faster and create original, effective solutions sooner.",
+  'images' => [
+    'images/Conductor_200x60-–-3.svg',
+    'images/Brightedge_200x60-–-1.svg',
+    'images/Botify_200x60.svg',
+    'images/GoogleSearchConsole_200x60-–-5.svg',
+    'images/DeepCrawl_200x60-–-4.svg',
+    'images/RioSEO_200x60-–-6.svg',
+    'images/BrightLocal_200x60-–-2.svg',
+    'images/GoogleAnalytics_200x60_Black-–-3.svg',
+  ]
+];
+
+$st = [
+  'name' => "Simeon O.",
+  'position' => "Product Manager, Phamatex Industries Limited",
+  'copy' => "There's no other company that does it better. Goral's professionalism, partnership and expertise was invaluable. They were a true extension of our team!"
+];
+
+
+?>
+<?php echo $__env->make('service-header',$sh, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-list',$sl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php echo $__env->make('service-cta',$sc, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php echo $__env->make('service-partners',$sp, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+																#block_60b01ac04c3d1 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01ac04c3d1" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row flex-row-reverse align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/se-1.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">SEO AUTHORITY</div>
+																							<h2 class="display-2 mb-4">For us, it’s personal.</h2>
+																											<div class="mb-4"><p>We’re a leading SEO authority. Our SEO leaders frequently speak and teach at established SEO conferences, boot camps, and training sessions, and have even been cited by Google. It’s not our nature to drop recommendations and disappear. We educate, empower, and work alongside your internal team.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+									<style>		
+																#block_60b015bf4c3d0 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b015bf4c3d0" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/se-2.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">GOOGLE EXPERTISE</div>
+																							<h2 class="display-2 mb-4">Expert E-A-T insights.</h2>
+																											<div class="mb-4"><p>As one of the first agencies to interpret Google’s E-A-T (expertise, authority, trust), our conclusions helped create industry-leading insights and principles. And we continue to increase our E-A-T authority through forward-thinking research and education.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+
+<?php echo $__env->make('service-client',$scl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-testimonial',$st, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-footer',['services' => $services], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-seo.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/c19ccdbe7b2f55df0b1be889fb97246bc2fabab2.php
@@ -0,0 +1,50 @@
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+	
+
+	<style>		
+					#block_60b04e256bbbb {
+				background: #7c0f81;
+			}
+							#block_60b04e256bbbb .text-box {
+				color: #ffffff;
+			}
+				
+	</style>
+
+</div></div><section id="block_60b04e256bbbb" class="content-block  alignfull testimonial-block testim_type-text py-5 my-4 background-7c0f81 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch testim-row">
+			 
+			<div class="col-md-10 offset-md-1 text-box py-md-5 py-4 my-auto">
+				<div class="row add-bord">
+					<div class="col-md-2 text-md-end text-start">
+						<svg version="1.1" class="mt-2 mb-3"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+							 y="0px" width="68.22px" height="55px" viewBox="0 0 108 86" enable-background="new 0 0 108 86" xml:space="preserve">
+						<g>
+							<g>
+								<path fill="#FFFFFF" d="M0,86c15.665,0,31.335,0,47,0c0-10.666,0-21.334,0-32c-8.333,0-16.667,0-25,0
+								c3.475-12.479,12.054-23.731,17-35c2.726-6.211,3.802-12.401,8-17c0-0.667,0-1.333,0-2C31.335,0,15.665,0,0,0
+								C0,28.664,0,57.336,0,86z M108,2c0-0.667,0-1.333,0-2C92.335,0,76.665,0,61,0c0,28.664,0,57.336,0,86c15.665,0,31.335,0,47,0
+								c0-10.666,0-21.334,0-32c-8.333,0-16.667,0-25,0c3.475-12.479,12.054-23.731,17-35C102.726,12.789,103.802,6.599,108,2z"/>
+							</g>
+						</g>
+						</svg>
+					</div>
+					<div class="col-md-10">
+						<div class="h3">“<?php echo e($copy); ?>”</div>
+													<div class="pt-4">
+								<div class="pb-2"><b><?php echo e($name); ?></b></div>
+								<div class=""><?php echo e($position); ?></div>
+							</div>
+							
+					</div>
+				</div>
+			</div>
+			 
+		</div>		
+	</div>
+</section><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-testimonial.blade.php ENDPATH**/ ?>

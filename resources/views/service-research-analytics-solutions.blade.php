50ea5f992a13d0b19e267c6b1580d09655fd5d5c
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-09-09 13:50:31 +0100

    Update service-research-analytics-solutions.blade.php

--- resources/views/service-research-analytics-solutions.blade.php
+++ resources/views/service-research-analytics-solutions.blade.php
@@ -11,19 +11,13 @@ $pcClass = "";
 @section('bodyClass',"page-paid-media noawesome hashero-color hashero")
 
 @section('content')
-<section id="block_607cddc420334" class="content-block d-flex  alignwide adtheme-hero align-items-end mob-stack color-as-herotype height-def has-background background-ffffff has-font-color text-default">
-	<div class="container-fluid mw-xl hero-block-content pb-sm-4">
-		<div class="row h-100">
-			<div class="col-md-7 text-start my-sm-3 my-4 text-white ">
-																	<div class="label">Research + Analytics Solutions</div>
-												<h1 class="hero-title display-1">Market Research + Analytics</h1>						
-				<div class="d-none d-sm-block">
-											<div class="hero-block-description py-3 h5">Great business decisions demand accurate data. Our proprietary first-party database and predictive modeling solutions can future-proof your business — cookie-free. 
-</div>
-														
-				</div>
-			</div>
-					</div>		
-	</div>
-</section>
+<?php
+$sh = [
+  'subtitle' => "Research + Analytics Solutions",
+  'title' => "Market Research + Analytics",
+  'copy' => "Great business decisions demand accurate data. Our proprietary first-party database and predictive modeling solutions can future-proof your business — cookie-free."
+];
+?>
+@include('service-header',$sh)
+
 @stop

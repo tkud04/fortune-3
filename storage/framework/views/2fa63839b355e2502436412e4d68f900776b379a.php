76054ab335d611fabcf64800330fe31b8bf40e9f
Tkud <kudayisitobi@gmail.com>
2021-09-09 20:31:01 +0100

    rounding up

---
+++ storage/framework/views/2fa63839b355e2502436412e4d68f900776b379a.php
@@ -0,0 +1,166 @@
+<?php
+$pcClass = "";
+?>
+
+
+
+<?php $__env->startSection('title',"Performance Creative"); ?>
+
+
+
+<?php $__env->startSection('bodyClass',"page-paid-media noawesome hashero-color hashero"); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php
+$sh = [
+  'subtitle' => "PERFORMANCE CREATIVE",
+  'title' => "Impactful creative — powered by data.",
+  'copy' => "Performance creative demands getting your hands dirty digging into raw data. This data fuels passionate, impactful concepts — the perfect blend of art and science."
+];
+
+$sl = [
+  'title_1' => "what we do",
+  'title_2' => "Outperform on your customers’ cross-channel brand journeys.",
+  'title_3' => "Our creative aligns powerful concepts with SEO, media, and UX best practices to inform, educate, and inspire your audience.",
+  'list' => [
+  ['title' => "Display",'copy' => "Engage effortlessly with prospects to increase purchase intent by contextually targeting audiences with our propriety database and data partnerships."],
+  ['title' => "Social",'copy' => "Concept and construct eye-catching creative that introduces your brand, builds interest, and generates purchase intent."],
+  ['title' => "Video",'copy' => "Distribute multivariate-tested, high-impact motion graphics across key channels to drive excitement and demand for your products."],
+  ['title' => "Website + Experience Design",'copy' => "We design, develop and manage SEO-optimized, user-centric websites and applications with strong SEO integration and foundations that achieve business outcomes."],
+  ]
+];
+
+$sc = [
+'subtitle' => "OUR APPROACH",
+  'title' => "High-impact creative.",
+  'copy' => "<p>Right message, right moment, right media — we vigorously test and collaborate across our specialist teams to discover the perfect combination of messaging, timing, and media to inform and initiate action. Let your brand speak and cut through the noise to educate and inspire your ideal customer. </p>"
+];
+
+$scl = [
+  'img' => "images/dangote.jpg",
+  'title' => "Dangote Sugar Refinery Limited",
+  'num' => "+6.7x",
+  'subtitle' => "Website traffic",
+  'copy' => "We redesigned the company's corporate website to maximize the overall user experience. We optimized the website load speeds to the most minimum to enable visits even with sub-par network coverage. The design direction maintained a sleek, cleanly designed corporate website with superior minimalist graphics."
+];
+
+$sp = [
+  'title' => "We know the right people.",
+  'subtitle' => "OUR PARTNERS",
+  'copy' => "We’ve built elite partnerships and strong relationships with leading SEO institutions. These partnerships grant us enterprise-level technology and insights to know more, pivot faster and create original, effective solutions sooner.",
+  'images' => [
+    'images/Conductor_200x60-–-3.svg',
+    'images/Brightedge_200x60-–-1.svg',
+    'images/Botify_200x60.svg',
+    'images/GoogleSearchConsole_200x60-–-5.svg',
+    'images/DeepCrawl_200x60-–-4.svg',
+    'images/RioSEO_200x60-–-6.svg',
+    'images/BrightLocal_200x60-–-2.svg',
+    'images/GoogleAnalytics_200x60_Black-–-3.svg',
+  ]
+];
+
+$st = [
+  'name' => "Simeon O.",
+  'position' => "Product Manager, Phamatex Industries Limited",
+  'copy' => "There's no other company that does it better. Goral's professionalism, partnership and expertise was invaluable. They were a true extension of our team!"
+];
+
+
+?>
+<?php echo $__env->make('service-header',$sh, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-list',$sl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php echo $__env->make('service-cta',$sc, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+									<style>		
+																#block_60b015bf4c3d0 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b015bf4c3d0" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/service-footer.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label"></div>
+																							<h2 class="display-2 mb-4">We don’t do guesswork.</h2>
+																											<div class="mb-4">
+																											<p>Our testing and iteration use the latest analytical tools to pinpoint insights, optimize best-performing content, and maximize ROI.</p>
+																											<p>Experimentation is necessary — and we’ve got it down to a science. We don’t waste your time or ad spend testing variables that might work. Our data partnerships and tools tell us what will work. And then we optimize our creative to work even better.</p>
+																											<ul>
+																											<li>Defining outcomes</li>
+																											<li>Auditing existing content</li>
+																											<li>Identifying opportunities</li>
+																											<li>Concepting creative</li>
+																											<li>Mapping + Production</li>
+																											<li>Analyzing performance</li>
+																											<li>Optimizing amplification</li>
+																											</ul>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+																#block_60b01ac04c3d1 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01ac04c3d1" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row flex-row-reverse align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/spc-1.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label"></div>
+																							<h2 class="display-2 mb-4">We’ve got “U.”</h2>
+																											<div class="mb-4">
+																											<p>We only design user-centric websites and solutions with leading UX and UI practices. From in-depth research, we recommend the best digital experience based on user journey needs and conversion rate optimization principles that directly improves conversions and engagement.</p>
+																											<p>Applying modern digital branding and design, we create a stunning online experience that resonates and communicates effectively with your target audience. To deliver on objectives and outcomes, we employ website and application best practices to enrich user experiences that deliver results.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+
+
+<?php echo $__env->make('service-client',$scl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-footer',['services' => $services], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-performance-creative.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4933cbce03c7e7f0a9b64c5e7b52747a584a0441.php
@@ -0,0 +1,155 @@
+<?php
+$pcClass = "";
+?>
+
+
+
+<?php $__env->startSection('title',"Strategy"); ?>
+
+
+
+<?php $__env->startSection('bodyClass',"page-paid-media noawesome hashero-color hashero"); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php
+$sh = [
+  'subtitle' => "Strategy",
+  'title' => "Business-building strategy.",
+  'copy' => "From the first kick-off meeting to years of partnership, great thinkers continually evolve with technology and always finish first."
+];
+
+$sl = [
+  'title_1' => "what we do",
+  'title_2' => "Our strategic thinking elevates your business outcomes.",
+  'title_3' => "",
+  'list' => [
+  ['title' => "Media Planning",'copy' => "Dive deep into your growth goals, business processes, overall client acquisition strategy, client nurturing strategy, and more to develop a holistic digital advertising strategy."],
+  ['title' => "Audience Development",'copy' => "Educate, engage, and inspire loyal and new audiences throughout their entire customer journey based directly on real-time data."],
+  ['title' => "Go-to-Market Strategy",'copy' => "Accelerate revenue and increase product reach with a comprehensive plan that delivers measurable, actionable goals and results."],
+  ['title' => "Digital Marketing Consulting",'copy' => "Facing a complex business challenge is our favorite. We help clients tackle tough problems and face unknown changes in their industry or the world at large."],
+  ['title' => "Training",'copy' => "Our specialists work side-by-side with brands’ internal teams to educate them on best practices and updates for every channel and platform."],
+  ['title' => "Brand Positioning, Messaging + Creative Strategy",'copy' => "Set your brand up for success. We help identify your brand’s unique position and key messaging and develop top-performing creative strategy to engage your audience at every customer stage."],
+  ]
+];
+
+$sc = [
+'subtitle' => "OUR APPROACH",
+  'title' => "Anticipate. Act. Repeat.",
+  'copy' => "<p>At every step of the consumer journey, we utilize up-to-date insights that seamlessly guide target customers through your branded digital experience. We intentionally audit for strengths and pain points and problem solve as a team. More than just maintaining growth, our solutions fuel it.  </p>"
+];
+
+$scl = [
+  'img' => "images/dangote.jpg",
+  'title' => "Dangote Sugar Refinery Limited",
+  'num' => "+6.7x",
+  'subtitle' => "Website traffic",
+  'copy' => "We redesigned the company's corporate website to maximize the overall user experience. We optimized the website load speeds to the most minimum to enable visits even with sub-par network coverage. The design direction maintained a sleek, cleanly designed corporate website with superior minimalist graphics."
+];
+
+$sp = [
+  'title' => "We know the right people.",
+  'subtitle' => "OUR PARTNERS",
+  'copy' => "We’ve built elite partnerships and strong relationships with leading SEO institutions. These partnerships grant us enterprise-level technology and insights to know more, pivot faster and create original, effective solutions sooner.",
+  'images' => [
+    'images/Conductor_200x60-–-3.svg',
+    'images/Brightedge_200x60-–-1.svg',
+    'images/Botify_200x60.svg',
+    'images/GoogleSearchConsole_200x60-–-5.svg',
+    'images/DeepCrawl_200x60-–-4.svg',
+    'images/RioSEO_200x60-–-6.svg',
+    'images/BrightLocal_200x60-–-2.svg',
+    'images/GoogleAnalytics_200x60_Black-–-3.svg',
+  ]
+];
+
+$st = [
+  'name' => "Simeon O.",
+  'position' => "Product Manager, Phamatex Industries Limited",
+  'copy' => "There's no other company that does it better. Goral's professionalism, partnership and expertise was invaluable. They were a true extension of our team!"
+];
+
+
+?>
+<?php echo $__env->make('service-header',$sh, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-list',$sl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+
+<?php echo $__env->make('service-cta',$sc, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+									<style>		
+																#block_60b015bf4c3d0 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b015bf4c3d0" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/sst-1.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">OUR MINDSET</div>
+																							<h2 class="display-2 mb-4">Numbers first. Ego second.</h2>
+																											<div class="mb-4">
+																											<p>Our data partnerships tell us exactly who your ideal customers are, where to find them, how to connect with them, and most importantly, what’s working right now. And we shift resources towards even better solutions to achieve the best results.</p>
+																											
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+																#block_60b01ac04c3d1 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01ac04c3d1" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row flex-row-reverse align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/sst-2.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">OUR CULTURE</div>
+																							<h2 class="display-2 mb-4">We think bigger.</h2>
+																											<div class="mb-4">
+																											<p>Smart, talented, passionate digital experts, innovators, and specialists who empower and learn from each other — that’s us. Strategic thinking is integral to our culture. From organic social creative to local SEO tactics, every decision we make has been thought out and thought through as customer-centric solutions for our clients.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+<?php echo $__env->make('service-footer',['services' => $services], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-strategy.blade.php ENDPATH**/ ?>

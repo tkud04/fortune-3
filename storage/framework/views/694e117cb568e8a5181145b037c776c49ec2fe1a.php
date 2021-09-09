93ce874a6072523606f8e0230f2d488a25790ced
Tkud <kudayisitobi@gmail.com>
2021-09-09 17:15:37 +0100

    rounding up

---
+++ storage/framework/views/694e117cb568e8a5181145b037c776c49ec2fe1a.php
@@ -0,0 +1,217 @@
+<?php
+$pcClass = "";
+?>
+
+
+
+<?php $__env->startSection('title',"Paid Media"); ?>
+
+
+
+<?php $__env->startSection('bodyClass',"page-paid-media noawesome hashero-color hashero"); ?>
+
+<?php $__env->startSection('content'); ?>
+<?php
+$sh = [
+  'subtitle' => "Paid Media",
+  'title' => "Outperform with paid media.",
+  'copy' => "Our holistic digital strategies, proprietary insights, and granular targeting accelerate growth and outmaneuver your competition."
+];
+
+$sl = [
+  'title_1' => "what we do",
+  'title_2' => "Develop a customized, full-funnel media mix to amplify ad budget and outperform in your market.",
+  'title_3' => "",
+  'list' => [
+  ['title' => "Paid Search",'copy' => "Reach brand-aware consumers at the right moment with targeted and actionable advertising. Engage prospects or increase brand loyalty with exact audience targeting — down to just one household."],
+  ['title' => "Paid Social",'copy' => "Build and nurture a relationship between your brand and best customers using surround-sound social media strategies and top-performing organic creative."],
+  ['title' => "Shopping",'copy' => "Reach loyal and new customers through holistic product campaigns and integrated strategies at every decision touchpoint."],
+  ['title' => "Amazon + Retail",'copy' => "Be first in line to meet your ideal customers with our expert team and enterprise-level data partnerships."],
+  ['title' => "Video + OTT",'copy' => "Gain brand awareness with new audiences and develop transparent, performance-driven advertising."],
+  ]
+];
+
+$sc = [
+'subtitle' => "OUR APPROACH",
+  'title' => "Holistic + transparent approach to maximize returns.",
+  'copy' => "<p>From identifying business goals to retargeting performance, our expert team communicates directly with yours. We proactively adapt to any changes in goals and strategies, campaign results, the marketplace. Our team, while always growing, is dedicated and close-knit so you can work with the same people year after year.</p>"
+];
+?>
+<?php echo $__env->make('service-header',$sh, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-list',$sl, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-cta',$sc, \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<div><div class="container-fluid mw-xl">
+
+	
+
+	<style>
+					#block_60b015a84c3cf {			
+				background: #f9f9f9;					
+			}	
+			
+					#block_60b015a84c3cf * {
+				color: #000000;
+			}
+				</style>
+
+</div></div><section id="block_60b015a84c3cf" class="content-block  alignfull info-block py-md-5 py-4 background-f9f9f9 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row py-md-2 ">
+			<div class="col-md-6 py-md-4">
+				<div class="me-md-5 pe-md-5">
+				  <h2 class="cta-title h2 mb-0">Predictive modelling + audience targeting. </h2>													
+				</div>
+			</div>
+			<div class="col-md-6 py-md-4">
+			 <div class="mb-0 px-md-2 py-md-0 py-4 text-base"><p>We target down to a single household. Leverage our in-house database of 10M+ customer profiles and 2,000+ customer variables to identify your exact customers at every stage of their consumer journey.</p></div>
+			</div>
+		</div>		
+	</div>
+</section>
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+									<style>		
+																#block_60b015bf4c3d0 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b015bf4c3d0" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/spm-1.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">AWARENESS</div>
+																							<h2 class="display-2 mb-4">Build brand awareness for products and services.</h2>
+																											<div class="mb-4"><p>Introduce your brand with social, native, video, and display advertising. From TikTok to YouTube and LinkedIn to Google’s display network, meet your ideal consumer from every digital angle.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+																#block_60b01ac04c3d1 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01ac04c3d1" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row flex-row-reverse align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/spm-2.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">CONSIDERATION</div>
+																							<h2 class="display-2 mb-4">Reach hand-raisers and in-market consumers.</h2>
+																											<div class="mb-4"><p>Nurture interest and engagement with strategic retargeting through consumers’ favorite platforms, whether it’s a podcast or OOT advertising on their favorite viewing platform. Create a holistic search user experience. When your target audience is ready to learn more — your brand has all the answers.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+																#block_60b01afc4c3d3 .text-box * {
+							color: #000000 !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01afc4c3d3" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row  align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/spm-4.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">CONVERSION</div>
+																							<h2 class="display-2 mb-4">Encourage action from your most valuable prospects.</h2>
+																											<div class="mb-4"><p>We achieve optimal conversions through strategic retargeting through all capable channels. From shopping to search to social, we create a clear moment of action for your customer.</p>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+<div><div class="container-fluid mw-xl">
+
+									<style>		
+											#block_60b01ae84c3d2 {
+							background: #7c0f81;
+						}
+																#block_60b01ae84c3d2 .text-box * {
+							color: #ffffff !important;
+						}
+							
+				</style>
+						
+	
+</div></div><section id="block_60b01ae84c3d2" class="content-block  alignfull image-desc my-0 has-font-color text-default">	<div class="container-fluid mw-xl">
+		<div class="row flex-row-reverse align-items-stretch h-100">
+			<div class="col-lg-7 color-box align-self-stretch px-0">
+									<div data-bg="images/spm-3.jpg" class="image-background h-100 rocket-lazyload" 
+						aria-hidden="true" 
+						style="">
+					</div>
+							</div>
+			<div class="col-lg-5 text-box  py-md-5 py-4">
+				<div class="py-4 px-md-4 mx-1 d-flex h-100 align-items-center">
+					<div>
+											<div class="label">TESTING + OPTIMIZATION</div>
+																							<h2 class="display-2 mb-4">We don’t do guesswork.</h2>
+																											<div class="mb-4"><p>Experimentation is necessary — but we’ve got it down to a science. Using the latest analytics tools, we test and iterate to pinpoint insights, optimize best-performing content, and maximize your ROI.</p>
+<ul>
+<li>Defining Outcomes</li>
+<li>Auditing Existing Content</li>
+<li>Identifying Opportunities</li>
+<li>Concepting Creative</li>
+<li>Mapping + Production</li>
+<li>Analyzing Performance</li>
+<li>Optimizing Amplification</li>
+</ul>
+</div>
+															</div>
+				</div>
+			</div>		
+		</div>		
+	</div>
+</section>
+
+
+<?php echo $__env->make('service-client',[], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->make('service-footer',['services' => $services], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-paid-media.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e347074c75008d811c6ca9aa7d2ac72c904531b4.php
@@ -0,0 +1,39 @@
+<div><div class="container-fluid mw-xl">
+
+
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+
+
+</div></div>
+<section id="block_60b072f4850f5" class="content-block  alignfull content-grid py-0 my-0 has-font-color text-default">
+  <div class="row mb-0 grid-line mx-0">
+	<div class="col-12 single-grid g-type-1 text-center mb-0 p-0">
+						
+		<div class="grid-in  withtext islinked custom-grid force-img h-100 uselarge" >
+		<a href="javascript:void(0)" class="grid-link" ><span class="visually-hidden">The Sak</span></a>		
+		<div class="image-background" aria-hidden="true">
+				<picture class="zoom-img">
+<source type="image/webp" data-lazy-srcset="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png.webp 1950w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1200x400.png.webp 1200w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-600x200.png.webp 600w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-768x256.png.webp 768w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1536x512.png.webp 1536w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-2048x683.png.webp 2048w" srcset="data:image/svg+xml,%3Csvg%20xmlns=&#039;http://www.w3.org/2000/svg&#039;%20viewBox=&#039;0%200%201170%20390&#039;%3E%3C/svg%3E" data-lazy-sizes="(max-width: 1170px) 100vw, 1170px"/>
+<img width="1170" height="390" src="data:image/svg+xml,%3Csvg%20xmlns=&#039;http://www.w3.org/2000/svg&#039;%20viewBox=&#039;0%200%201170%20390&#039;%3E%3C/svg%3E" alt="TheSak_CS_3x1@2x" data-lazy-srcset="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png 1950w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1200x400.png 1200w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-600x200.png 600w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-768x256.png 768w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1536x512.png 1536w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-2048x683.png 2048w" data-lazy-sizes="(max-width: 1170px) 100vw, 1170px" data-lazy-src="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png"/>
+</picture>
+<noscript><picture class="zoom-img">
+<source type="image/webp" srcset="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png.webp 1950w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1200x400.png.webp 1200w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-600x200.png.webp 600w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-768x256.png.webp 768w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1536x512.png.webp 1536w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-2048x683.png.webp 2048w" sizes="(max-width: 1170px) 100vw, 1170px"/>
+<img width="1170" height="390" src="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png" alt="TheSak_CS_3x1@2x" srcset="https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1950x650.png 1950w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1200x400.png 1200w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-600x200.png 600w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-768x256.png 768w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-1536x512.png 1536w, https://www.amsivedigital.com/wp-content/uploads/2021/04/TheSak_CS_3x1@2x-2048x683.png 2048w" sizes="(max-width: 1170px) 100vw, 1170px"/>
+</picture>
+</noscript>			
+     </div>
+			
+			
+		<div class="text-start text-white row mx-md-4 text-top h-100 mx-0">	
+			<div class="col-lg-4 col-md-6 col-10 offset-1 offset-md-0 py-5 my-auto">
+									<p class="gr-t mb-lg-4 mb-3 titillium-web font-weight-normal">The Sak</p>
+													<div class="gr-sub display-2 text-white mb-lg-4 mb-3">10x</div>
+													<p class="gr-cat text-start grid-cat pe-lg-5 me-lg-5">Ecommerce Growth</p>
+													<div class="text-start small-font">Driven by both established and new customers, we created a strategy that supported moving unsold merchandise while creating an entirely new level of brand demand. </div>
+							</div>	
+		</div>		
+	</div>
+																
+	</div>
+  </div>	
+</section><?php /**PATH C:\bkupp\lokl\repo\fortune-3\resources\views/service-client.blade.php ENDPATH**/ ?>
